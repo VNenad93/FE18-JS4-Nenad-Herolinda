2d8a7a2eeef7b28e1b4a59ef1b77157b61d3eaf0
Nenad Vasiljev <vasiljevnenad1@gmail.com>
2023-02-07 13:48:52 +0100

    challenge finished

--- Vasiljev/Challenge/js.js
+++ Vasiljev/Challenge/js.js
@@ -3,15 +3,25 @@ let player2 = document.getElementById('player2')
 
 let dice1 = document.getElementById('dice1')
 let dice2 = document.getElementById('dice2')
-
+let result = document.getElementById('result')
 
 dice1.addEventListener('click', () => {
     let random = Math.floor(Math.random() * 8)
-    player1.innerText = random
+    player1.textContent = random;
 })
-
 
 dice2.addEventListener('click', () => {
     let random = Math.floor(Math.random() * 8)
-    player2.innerText = random
+    player2.textContent = random;
+    decide()
 })
+
+function decide() {
+    if(player1.textContent > player2.textContent) {
+        result.textContent = 'Player 1 Won'
+    } else if(player1.textContent < player2.textContent) {
+        result.textContent = 'Player 2 Won' 
+    } else (
+        result.textContent = 'Draw'
+    )
+}
